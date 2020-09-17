dbb4b5c9585eee21ba245c759c8f4667d977702f
Garvit Khurana <55339445+khuranagarvit019@users.noreply.github.com>
2020-09-17 22:33:32 +0530

    Create Day:2 Operators.cpp

--- Day 2: Operators.cpp
+++ Day 2: Operators.cpp
@@ -7,14 +7,13 @@ int main() {
     int tipPercent;
     int taxPercent;
     double mealCost;
-    scanf("%lf %d %d", &mealCost, &tipPercent, &taxPercent);
-    // OR: cin >> mealCost >> tipPercent >> taxPercent;
+    cin >> mealCost >> tipPercent >> taxPercent;
     
     double tip = mealCost * tipPercent / 100;
     double tax = mealCost * taxPercent / 100;
     int totalCost = round(mealCost + tip + tax);
     
-    printf("The total meal cost is %d dollars.", totalCost);
+    cout<<totalCost;
     
     return 0;
 }
