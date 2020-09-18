b57e1104c887a1fc8ca604cebaf0da2bbaf10249
Garvit Khurana <55339445+khuranagarvit019@users.noreply.github.com>
2020-09-18 15:33:55 +0530

    Create Day3: Intro To Conditional Statements.cpp

---
+++ Day3: Intro To Conditional Statements.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    if( N%2 == 1 || (N > 5 && N < 21)){
+        cout<<"Weird";
+    }
+    else{
+        cout<<"Not Weird";
+    }
+
+    return 0;
+}
