bfbeb4cf47540492d4b7709f3f04587e3469d982
Garvit Khurana <55339445+khuranagarvit019@users.noreply.github.com>
2020-10-04 22:09:34 +0530

    Update Day 19: Interfaces.cpp

--- Day 19: Interfaces.cpp
+++ Day 19: Interfaces.cpp
@@ -12,12 +12,12 @@ class AdvancedArithmetic{
 class Calculator : public AdvancedArithmetic {
 public:
         int divisorSum(int n) {
-        int total=0,i;
-        for(i=1; i<=n; i++) { 
-            if(n%i == 0) { 
-            total+=i; 
+            int total=0,i;
+            for(i=1; i<=n; i++) { 
+                if(n%i == 0) { 
+                total+=i; 
+                }
             }
-        }
         return total;
     }
 };
